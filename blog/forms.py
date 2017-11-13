620f37dde939911e007818b5e78aeb0fab392240
jeanettie <jeanettienicole@gmail.com>
2017-11-13 11:51:53 -0500

    adding delete to project

--- blog/forms.py
+++ blog/forms.py
@@ -7,3 +7,10 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+
+
+class DeleteForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['author', 'title', 'text', 'created_date', 'published_date' ]
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from django.shortcuts import render, get_object_or_404, redirect
 
 from .models import Post
-from .forms import PostForm
+from .forms import PostForm, DeleteForm
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
@@ -39,3 +39,13 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_delete(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_delete.html', {'post': post})
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
+
